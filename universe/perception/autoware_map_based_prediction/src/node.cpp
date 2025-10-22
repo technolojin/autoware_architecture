d07374b50dc9e83edad073c52231838bbe2fea54
Taekjin LEE <taekjin.lee@tier4.jp>
2025-10-22 14:59:20 +0900

    feat: Add MapBasedPredictionNode with CMake, YAML configurations, and schema definitions

---
+++ universe/perception/autoware_map_based_prediction/src/node.cpp
@@ -0,0 +1,35 @@
+#include <rclcpp/rclcpp.hpp>
+#include <chrono>
+
+namespace autoware::map_based_prediction
+{
+class MapBasedPredictionNode : public rclcpp::Node
+{
+public:
+  explicit MapBasedPredictionNode(const rclcpp::NodeOptions & options)
+  : Node("map_based_prediction", options)
+  {
+    // Create a timer that calls the callback every 2 seconds (1/2 Hz)
+    timer_ = this->create_wall_timer(
+      std::chrono::seconds(2),
+      std::bind(&MapBasedPredictionNode::timer_callback, this));
+    
+    RCLCPP_INFO(this->get_logger(), "MapBasedPredictionNode started");
+  }
+
+private:
+  void timer_callback()
+  {
+    // Publish node namespace with node name
+    std::string node_namespace = this->get_namespace();
+    std::string node_name = this->get_name();
+    RCLCPP_INFO(this->get_logger(), "Node %s %s", node_namespace.c_str(), node_name.c_str());
+  }
+
+  rclcpp::TimerBase::SharedPtr timer_;
+};
+}
+
+#include <rclcpp_components/register_node_macro.hpp>
+
+RCLCPP_COMPONENTS_REGISTER_NODE(autoware::map_based_prediction::MapBasedPredictionNode)
